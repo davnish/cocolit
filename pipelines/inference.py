82c9ab860b3589ff9d6c4a8947994e8fc169cd55
davnish <nischal.singh38@gmail.com>
2025-05-01 01:08:01 +0530

    [fix] inference.py to use temp dir, [add] augmentations in train.py

--- pipelines/inference.py
+++ pipelines/inference.py
@@ -7,6 +7,7 @@ from src.valid_bbox import valid_bbox
 from pathlib import Path
 import geopandas as gpd
 import tempfile
+import shutil
 
 import logging
 
@@ -18,6 +19,7 @@ logger = logging.getLogger(__name__)
 def inference(
         bbox: dict,
         model_path: str | None = None, 
+        temp_dir: str | None = None
         ) -> Path:
     """
     This is a pipeline for inference on finetunned YOLOv11 model.
@@ -32,30 +34,32 @@ def inference(
         valid_bbox(bbox)
         logging.info(f"Valid bounding box : {bbox.to_crs(epsg=4326).bounds.to_numpy().tolist()[0]}")
 
-        with tempfile.NamedTemporaryFile(delete=True, suffix = '.tif') as temp_file:
-            image_path = Path(temp_file.name)
-        logging.info(f"Temporary image path created: {image_path}")
+        # with tempfile.TemporaryDirectory(dir = '.') as temp_dir:
+        if temp_dir is None:
+            temp_dir = Path(tempfile.mkdtemp(dir = '.'))
+        temp_name = 'image'
 
+        logging.info(f"Temporary directory created: {temp_dir}")
+
+        image_path = temp_dir / (temp_name + '.tif')
+        logging.info(f"Temporary image path created: {image_path}")
 
         data = TMStoGeoTIFF(output=image_path, bbox=bbox.to_crs(epsg=4326).bounds.to_numpy().tolist()[0]) ###### This will change after db integration. After each prediction we will update the name of the file.
         data.download()
         logging.info(f"Image downloaded: {image_path}")
 
-        with tempfile.TemporaryDirectory() as temp_txt_file:
-            txt_dir_path = Path(temp_txt_file)
-        logging.info(f"Temporary txt directory created: {txt_dir_path}")
-
-        model = YOLO("yolo11n.pt")
+        model = YOLO(model_path)
         logging.info(f"Model loaded")
 
-        model.predict(source=image_path, save=True, save_txt=True, project=txt_dir_path)
-        logging.info(f"Prediction done. Text files saved to: {txt_dir_path}")
+        model.predict(source=image_path, save_txt=True, project=image_path.parent)
+        logging.info(f"Prediction done. Text files saved to: {temp_dir}/predict/labels/{temp_name + '.txt'}")
 
-        with tempfile.TemporaryDirectory() as temp_shp_file:
-            shp_dir_path = Path(temp_shp_file)
-        logging.info(f"Temporary shapefile directory created: {shp_dir_path}")
-
-        shp_path = txt_to_shp(txt_dir_path, image_path.parents, shp_dir_path)
+        shp_path = txt_to_shp(
+            txt_dir=temp_dir / f"predict/labels/{(temp_name + '.txt')}", 
+            img_dir=temp_dir, 
+            shp_dir=temp_dir
+            )
+        
         logging.info(f"Shapefile created at: {shp_path}")
         return shp_path
     
@@ -64,14 +68,14 @@ def inference(
         return None
 
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}", exc_info=True)
         return None
-
-
-
+    finally:
+        shutil.rmtree(temp_dir, ignore_errors=True)
+        logging.info(f"Temporary directory removed: {temp_dir}")
 
 
 if __name__ == "__main__":
-    data = {"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[74.347916,24.287027],[74.347916,24.293129],[74.355469,24.293129],[74.355469,24.287027],[74.347916,24.287027]]]}}
-    shp = inference(data)
+    data = {"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[80.020219,7.809309],[80.020219,7.810361],[80.021667,7.810361],[80.021667,7.809309],[80.020219,7.809309]]]}}
+    shp = inference(data, "models/train15/weights/best.pt")
     print(shp)
