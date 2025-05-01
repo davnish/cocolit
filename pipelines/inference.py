c3abe354dea308e646c731f58c90af7e337631cc
davnish <nischal.singh38@gmail.com>
2025-05-01 18:08:43 +0530

    [fix] made the inferece class based

--- pipelines/inference.py
+++ pipelines/inference.py
@@ -6,76 +6,92 @@ from src.txt_to_shp import txt_to_shp
 from src.valid_bbox import valid_bbox
 from pathlib import Path
 import geopandas as gpd
+from geopandas import GeoDataFrame
 import tempfile
 import shutil
+import time
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# converting to class
 
-
-def inference(
-        bbox: dict,
-        model_path: str | None = None, 
-        temp_dir: str | None = None
-        ) -> Path:
-    """
-    This is a pipeline for inference on finetunned YOLOv11 model.
-    """
-
-    # Create a temporary file to store the downloaded image
-    try:
-
-        bbox = get_shapefile(bbox)
-        logging.info("Geojson to Shapefile conversion done")
-
-        valid_bbox(bbox)
-        logging.info(f"Valid bounding box : {bbox.to_crs(epsg=4326).bounds.to_numpy().tolist()[0]}")
-
-        # with tempfile.TemporaryDirectory(dir = '.') as temp_dir:
-        if temp_dir is None:
-            temp_dir = Path(tempfile.mkdtemp(dir = '.'))
-        temp_name = 'image'
-
-        logging.info(f"Temporary directory created: {temp_dir}")
-
-        image_path = temp_dir / (temp_name + '.tif')
-        logging.info(f"Temporary image path created: {image_path}")
-
-        data = TMStoGeoTIFF(output=image_path, bbox=bbox.to_crs(epsg=4326).bounds.to_numpy().tolist()[0]) ###### This will change after db integration. After each prediction we will update the name of the file.
-        data.download()
-        logging.info(f"Image downloaded: {image_path}")
-
-        model = YOLO(model_path)
-        logging.info(f"Model loaded")
-
-        model.predict(source=image_path, save_txt=True, project=image_path.parent)
-        logging.info(f"Prediction done. Text files saved to: {temp_dir}/predict/labels/{temp_name + '.txt'}")
-
-        shp_path = txt_to_shp(
-            txt_dir=temp_dir / f"predict/labels/{(temp_name + '.txt')}", 
-            img_dir=temp_dir, 
-            shp_dir=temp_dir
-            )
-        
-        logging.info(f"Shapefile created at: {shp_path}")
-        return shp_path
+class InferencePipeline:
+    def __init__(
+            self, 
+            model_path: str,
+            )->None:
     
-    except InvalidBBox as e:
-        logger.error(f"Invalid bounding box: {e}")
-        return None
+        self.model = YOLO(model_path, task='detect')
 
-    except Exception as e:
-        logger.error(f"An error occurred: {e}", exc_info=True)
-        return None
-    finally:
-        shutil.rmtree(temp_dir, ignore_errors=True)
-        logging.info(f"Temporary directory removed: {temp_dir}")
+
+    def preprocess(self, data) -> list:
+        data = get_shapefile(data) # Thinking to shrink large bounding boxes
+        valid_bbox(data)
+
+        # Getting the bounds
+        data = data.to_crs(epsg=4326).bounds.to_numpy().tolist()[0]
+        return data
+
+    def get_paths(self) -> tuple[str, str, str]:
+        temp_name = 'image'
+        dir = Path(tempfile.mkdtemp(dir = '.'))
+        image_path = dir / (temp_name + '.tif')
+        txt_path = dir / f"predict/labels/{(temp_name + '.txt')}"
+
+        return dir, image_path, txt_path
+    
+    def rm_paths(self, dir : str) -> None:
+        shutil.rmtree(dir)
+    
+    def run(self, data : dict) -> GeoDataFrame:
+        try:
+            bbox = self.preprocess(data)
+            logger.info("Processed and validated bbox")
+
+            dir, image_path, txt_path = self.get_paths()
+
+            data = TMStoGeoTIFF(output=image_path, bbox=bbox) ###### This will change after db integration. After each prediction we will update the name of the file.
+            data.download()
+            logger.info("Downloaded the images")
+
+
+            res = self.model.predict(source=image_path, save_txt=True, project=dir)
+            logger.info(f"Inference done, preds saved at {txt_path}")
+
+            if res[0].boxes.shape[0]>0:
+                preds = txt_to_shp(
+                            txt_dir=txt_path, 
+                            img_dir=dir, 
+                            shp_dir=dir
+                            ).to_crs(epsg = 4326)
+            else:
+                return None
+            logger.info(f"txt to shp done, Total preds of trees {len(preds)}.")
+            return preds
+
+
+        except InvalidBBox as e:
+            logger.error(f"Invalid bounding box: {e}")
+            return None
+
+        except Exception as e:
+            logger.error(f"An error occurred: {e}", exc_info=True)
+            return None
+        
+        finally:
+            self.rm_paths(dir)
+            logger.info("Paths removed")
 
 
 if __name__ == "__main__":
     data = {"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[80.020219,7.809309],[80.020219,7.810361],[80.021667,7.810361],[80.021667,7.809309],[80.020219,7.809309]]]}}
-    shp = inference(data, "models/train15/weights/best.pt")
-    print(shp)
+    
+    # gdf = inference(data, "models/train15/weights/best.pt" )
+    # print(gdf)
+
+    inference = InferencePipeline("models/train15/weights/best.pt")
+    gdf = inference.run(data)
+    print(gdf)
