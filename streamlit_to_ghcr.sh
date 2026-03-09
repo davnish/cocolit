9976c20aa11bb36fe6927607bd839ff33e19de15
davnish <nischal.singh38@gmail.com>
2026-03-09 23:31:08 +0530

    upd: commit id to deploy

--- streamlit_to_ghcr.sh
+++ streamlit_to_ghcr.sh
@@ -2,10 +2,10 @@ set -e
 
 NAMESPACE=davnish
 TAG=$(git rev-parse --short HEAD)
-IMAGE_NAME=ghcr.io/$NAMESPACE/$NEW_IMAGE_NAME:$TAG
+IMAGE_NAME=ghcr.io/$NAMESPACE/streamlit-server:$TAG
 
 
-docker build --platform linux/amd64 -t $IMAGE_NAME -f streamlit.Dockerfile .
+docker buildx build --platform linux/amd64 -t $IMAGE_NAME -f streamlit.Dockerfile .
 
 # docker tag $IMAGE_NAME ghcr.io/$NAMESPACE/$IMAGE_NAME
 
