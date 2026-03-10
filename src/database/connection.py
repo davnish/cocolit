de48da7c1de9978f9e8c7a0b920d0870fd36a2a1
davnish <nischal.singh38@gmail.com>
2026-03-10 14:46:47 +0530

    add: streamlit deployment

--- src/database/connection.py
+++ src/database/connection.py
@@ -9,11 +9,11 @@ load_dotenv()
 def get_engine():
     if os.getenv("local_database"):
         connection_string = os.getenv("local_database")
-    elif os.environ.get("db_conn_host"):
-        SERVER = os.environ["db_conn_host"]
-        DATABASE = os.environ["db_conn_database"]
-        USERNAME = os.environ["db_conn_user"]
-        PASSWORD = os.environ["db_conn_password"]
+    elif os.environ.get("postgres-host"):
+        SERVER = os.environ["postgres-host"]
+        DATABASE = os.environ["postgres-database"]
+        USERNAME = os.environ["postgres-user"]
+        PASSWORD = os.environ["postgres-password"]
         connection_string = f"postgresql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}"
     else:
         return None
