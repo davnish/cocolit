4d4d5d4e608d4d6afd25b5a2191cb46075b0c4d0
davnish <nischal.singh38@gmail.com>
2026-03-10 19:37:25 +0530

    fix: env database variable names

--- src/database/connection.py
+++ src/database/connection.py
@@ -11,8 +11,8 @@ def get_engine():
         connection_string = os.getenv("local_database")
     elif os.environ.get("POSTGRES_HOST"):
         SERVER = os.environ["POSTGRES_HOST"]
-        DATABASE = os.environ["POSTGRES_DB"]
-        USERNAME = os.environ["POSTGRES_USER"]
+        DATABASE = os.environ["POSTGRES_DATABASE"]
+        USERNAME = os.environ["POSTGRES_USERNAME"]
         PASSWORD = os.environ["POSTGRES_PASSWORD"]
         connection_string = f"postgresql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}"
     else:
